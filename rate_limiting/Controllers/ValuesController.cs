aaed3604e663929514667e2b4fcf298b3c59cab1
imandicdev <30394236+milijanm@users.noreply.github.com>
2020-02-18 01:43:00 +0100

    Rate Limiting v1.0

---
+++ rate_limiting/Controllers/ValuesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace rate_limiting.Controllers
+{
+    [Route("greet/{name}")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        
+        [HttpGet]
+        public IActionResult Greet(string name)
+        => Ok($"HI, {name}!");
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        
+                       
+                   
+    }
+}
